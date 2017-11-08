06a419a947b766aafccc6283a55f850fe548c587
everettjf <everettjf@live.com>
2017-11-08 11:15:38 +0800

    opt

--- appletrace/appletrace/src/objc/hook_objc_msgSend.m
+++ appletrace/appletrace/src/objc/hook_objc_msgSend.m
@@ -71,45 +71,49 @@ void objc_msgSend_pre_call(RegState *rs, ThreadStack *threadstack, CallStack *ca
     if(!APTIsEnable())
         return;
     char *sel_name = (char *)rs->general.regs.x1;
+    
     // No More Work Here!!! it will be slow.
-    if(LOG_ALL_SEL || (sel_name > log_sel_start_addr && sel_name < log_sel_end_addr)) {
-        // bad code! correct-ref: https://github.com/DavidGoldman/InspectiveC/blob/299cef1c40e8a165c697f97bcd317c5cfa55c4ba/logging.mm#L27
-        void *object_addr = (void *)rs->general.regs.x0;
-        void *class_addr = zz_macho_object_get_class((id)object_addr);
-        if(!class_addr)
-            return;
-        
-        void *super_class_addr = class_getSuperclass(class_addr);
-        // KVO 2333
-        if(LOG_ALL_CLASS
-           || (
-               0
-//              || (object_addr > log_class_start_addr && object_addr < log_class_end_addr)
-              || (class_addr >= log_class_start_addr && class_addr <= log_class_end_addr)
-//              || (super_class_addr > log_class_start_addr && super_class_addr < log_class_end_addr)
-              )
-           ) {
-            memset(decollators, 45, 128);
-            if(threadstack->size * 3 >= 128)
-                return;
-            decollators[threadstack->size * 3] = '\0';
-//            char *class_name = class_getName(object_addr);
-            const char *class_name = object_getClassName(object_addr);
-            
-            if(APTIsIgnoredClass(class_name)){
-                STACK_SET(callstack, "is_ignored", class_name, char*);
-                return;
-            }
-            
-            unsigned long repl_len = strlen(class_name) + strlen(sel_name) + 10;
-            char *repl_name = malloc(repl_len);
-            snprintf(repl_name, repl_len, "[%s]%s",class_name,sel_name);
-            STACK_SET(callstack, "repl_name", repl_name, char*);
-            
-//            NSLog(@"pre %s",repl_name);
-            APTBeginSection(repl_name);
-        }
+    if(!(LOG_ALL_SEL || (sel_name > log_sel_start_addr && sel_name < log_sel_end_addr))) {
+        return;
     }
+    
+    // bad code! correct-ref: https://github.com/DavidGoldman/InspectiveC/blob/299cef1c40e8a165c697f97bcd317c5cfa55c4ba/logging.mm#L27
+    void *object_addr = (void *)rs->general.regs.x0;
+    void *class_addr = zz_macho_object_get_class((id)object_addr);
+    if(!class_addr)
+        return;
+    
+    void *super_class_addr = class_getSuperclass(class_addr);
+    // KVO 2333
+    if(!(LOG_ALL_CLASS
+       || (
+           0
+//              || (object_addr > log_class_start_addr && object_addr < log_class_end_addr)
+          || (class_addr >= log_class_start_addr && class_addr <= log_class_end_addr)
+//              || (super_class_addr > log_class_start_addr && super_class_addr < log_class_end_addr)
+          )
+       )) {
+        return;
+    }
+    
+    memset(decollators, 45, 128);
+    if(threadstack->size * 3 >= 128)
+        return;
+    decollators[threadstack->size * 3] = '\0';
+    const char *class_name = object_getClassName(object_addr);
+    
+    if(APTIsIgnoredClass(class_name)){
+        STACK_SET(callstack, "is_ignored", class_name, char*);
+        return;
+    }
+    
+    unsigned long repl_len = strlen(class_name) + strlen(sel_name) + 10;
+    char *repl_name = malloc(repl_len);
+    snprintf(repl_name, repl_len, "[%s]%s",class_name,sel_name);
+    STACK_SET(callstack, "repl_name", repl_name, char*);
+    
+//    NSLog(@"pre %s",repl_name);
+    APTBeginSection(repl_name);
 }
 
 void objc_msgSend_post_call(RegState *rs, ThreadStack *threadstack, CallStack *callstack) {
